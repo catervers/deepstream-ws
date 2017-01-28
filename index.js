166457f48ea8a4176e11144e4bf19e114d696075
neurogas <neurogas@gmail.com>
2017-01-28 14:29:35 +0100

    clean up dependencies and set up playground

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // Load env config
 var config = require('./env.json')
 
+var deepstream = require('deepstream.io-client-js');
+
+var deepstreamClient = deepstream(config.deepstream).login();
+
+function subscribe(event) {
+    deepstreamClient.event.subscribe( event, function( payload ){
+        console.log( event+": "+payload );
+    });
+}
+
+function emit(event,payload) {
+    client.event.emit(event, payload);
+}
